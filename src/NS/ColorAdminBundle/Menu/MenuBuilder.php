ecbd8cd45756c04431ffd18af5126f7fce494e09
Mark Woynarski <mark.woynarski@nobletdesign.com>
2016-03-11 13:00:46 -0600

    Add breadcrumbs

--- src/NS/ColorAdminBundle/Menu/MenuBuilder.php
+++ src/NS/ColorAdminBundle/Menu/MenuBuilder.php
@@ -23,15 +23,16 @@ class MenuBuilder
                 ->setAttribute('badge-style', 'danger')
                 ->addChild('Sub Menu')
                     ->addChild('Sub Menu 2', array('route'=>'homepage'));
+
         $menu->addChild('ns_color_admin.menu.sidebar', array('uri'=>'http://www.google.com'));
 
         $item = $menu->addChild('To change')
             ->setAttribute('icon', 'envelope')
             ->setAttribute('label', 'NEW')
             ->setAttribute('label-style', 'warning');
-        $item->addChild('Sub Menu');
-        $item->addChild('Sub Menu');
-        $item->addChild('Sub Menu');
+        $item->addChild('Sub Menu', array('uri'=>'http://www.google.com'));
+        $item->addChild('Sub Menu 2', array('uri'=>'http://www.google.com'));
+        $item->addChild('Sub Menu 3', array('uri'=>'http://www.google.com'));
 
         $menu->addChild('This menu', array('uri'=>'http://www.google.com'))
             ->setAttribute('icon', 'star')
