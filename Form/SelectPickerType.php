84fd859591bdab3f7fa5c2ffc6fc656fd3a031a4
Mark Woynarski <mark.woynarski@nobletdesign.com>
2016-10-12 15:35:44 -0500

    Added selectpicker type

---
+++ Form/SelectPickerType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NS\ColorAdminBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use \Symfony\Component\Form\AbstractType;
+use \Symfony\Component\Form\FormView;
+use \Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Description of KnobType
+ *
+ * @author mark
+ */
+class SelectPickerType extends AbstractType
+{
+    private $defaults;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(['style', 'search', 'simple']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr']['data-style'] = isset($options['style']) ? $options['style'] : 'btn-white';
+        $view->vars['attr']['data-live-search'] = isset($options['search']) ? $options['search'] : 'true';
+        $view->vars['attr']['class'] = (isset($view->vars['attr']['class']) ? $view->vars['attr']['class']:'').(isset($options['simple']) ? 'selectpicker':'selectpicker form-control');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+}
