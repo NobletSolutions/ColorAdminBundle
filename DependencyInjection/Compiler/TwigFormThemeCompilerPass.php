384b88abd1d9f6ef2c643307bcfa00a35095423a
Nathanael d. Noblet <nathanael@noblet.ca>
2016-12-20 17:10:54 -0700

    configure form themes automatically

---
+++ DependencyInjection/Compiler/TwigFormThemeCompilerPass.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnat
+ * Date: 20/12/16
+ * Time: 4:56 PM
+ */
+
+namespace NS\ColorAdminBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TwigFormThemeCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasParameter('twig.form.resources')) {
+            $resources = $container->getParameter('twig.form.resources');
+            if (!in_array('NSColorAdminBundle:Form:fields.html.twig', $resources)) {
+                $resources[] = 'NSColorAdminBundle:Form:fields.html.twig';
+
+                $container->setParameter('twig.form.resources', $resources);
+            }
+        }
+    }
+}
--- NSColorAdminBundle.php
+++ NSColorAdminBundle.php
@@ -2,8 +2,15 @@
 
 namespace NS\ColorAdminBundle;
 
+use NS\ColorAdminBundle\DependencyInjection\Compiler\TwigFormThemeCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NSColorAdminBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new TwigFormThemeCompilerPass());
+    }
 }
