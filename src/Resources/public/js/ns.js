6d21e0a8031a19b74672d760fb85a6016a7ab458
Nathanael d. Noblet <nathanael@noblet.ca>
2017-02-01 10:59:32 -0700

    added generic 'readmore' expandable text feature

--- src/Resources/public/js/ns.js
+++ src/Resources/public/js/ns.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
         $(el).mask($(el).data('mask'), {placeholder:$(el).data('placeholder')});
         $(el).parents('div.form-group').children('label').append(' <small class="text-info">'+$(el).data('mask')+'</small>');
     });
+
+    $('.readmore').each(function()
+    {
+        $(this).css({'max-height':$(this).data('max-height')});
+    });
+
+    $('.readmore-expander').on('click', function () {
+        var readme = $(this).prev('.readmore');
+        if (!this.expanded) {
+            readme.css({'overflow-y': 'visible', 'max-height': 'inherit'});
+            this.expanded = true;
+            $(this).find('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+        }
+        else {
+            readme.css({'overflow-y': 'hidden', 'max-height': readme.data('max-height')});
+            this.expanded = false;
+            $(this).find('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+        }
+    });
 });
