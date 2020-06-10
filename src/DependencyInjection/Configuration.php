5c9481bab9b749bb46c68259be0e83f3d882314a
Mark Woynarski <mark.woynarski@nobletdesign.com>
2020-06-10 15:13:53 -0600

    Fix configuration for compat with SF5

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('color_admin');
+        $treeBuilder = new TreeBuilder('color_admin');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
