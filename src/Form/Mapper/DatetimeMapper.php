3295a22ff29ee579bae1807ca835ad5d02c44887
Nathanael d. Noblet <nathanael@noblet.ca>
2020-06-25 13:01:38 -0600

    Ensure functions match interface

--- src/Form/Mapper/DatetimeMapper.php
+++ src/Form/Mapper/DatetimeMapper.php
@@ -2,25 +2,27 @@
 
 namespace NS\ColorAdminBundle\Form\Mapper;
 
-class DatetimeMapper implements \Symfony\Component\Form\DataMapperInterface
+use DateTime;
+use Symfony\Component\Form\DataMapperInterface;
+
+class DatetimeMapper implements DataMapperInterface
 {
     /**
-     * @param \DateTime|null    $viewData
-     * @param iterable $forms
+     * @param DateTime|null $viewData
+     * @param iterable      $forms
      */
-    public function mapDataToForms($viewData, iterable $forms)
+    public function mapDataToForms($viewData, $forms)
     {
-        if($viewData !== null)
-        {
+        if ($viewData !== null) {
             $forms = iterator_to_array($forms);
             $forms['date']->setData($viewData);
             $forms['time']->setData($viewData);
         }
     }
 
-    public function mapFormsToData(iterable $forms, &$viewData)
+    public function mapFormsToData($forms, &$viewData)
     {
-        $forms = iterator_to_array($forms);
-        $viewData = \DateTime::createFromFormat('Y-m-d H:i:s', $forms['date']->getData()->format('Y-m-d').' '.$forms['time']->getData()->format('H:i:s'));
+        $forms    = iterator_to_array($forms);
+        $viewData = DateTime::createFromFormat('Y-m-d H:i:s', $forms['date']->getData()->format('Y-m-d') . ' ' . $forms['time']->getData()->format('H:i:s'));
     }
 }
