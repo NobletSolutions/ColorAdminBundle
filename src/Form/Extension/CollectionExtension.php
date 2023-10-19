0855eca43f7a631adde6b32b72f0f9a8be3d422c
Nathanael d. Noblet <nathanael@noblet.ca>
2023-10-18 23:15:21 -0600

    Added conditional to display add button for collections

--- src/Form/Extension/CollectionExtension.php
+++ src/Form/Extension/CollectionExtension.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace NS\ColorAdminBundle\Form\Extension;
-
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -15,13 +13,18 @@ class CollectionExtension extends AbstractTypeExtension
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefined(['add_button_label', 'add_button_icon']);
-        $resolver->setDefaults(['add_button_label' => 'Add', 'add_button_icon' => 'plus']);
+        $resolver->setDefaults([
+            'add_button'       => true,
+            'add_button_label' => 'Add',
+            'add_button_icon'  => 'plus'
+        ]);
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
+        $view->vars['add_button']       = $options['add_button'];
         $view->vars['add_button_label'] = $options['add_button_label'];
-        $view->vars['add_button_icon'] = $options['add_button_icon'];
+        $view->vars['add_button_icon']  = $options['add_button_icon'];
     }
 
     /**
